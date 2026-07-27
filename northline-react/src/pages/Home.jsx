cdb815b10d9b1e1d5279349fcd79a761e76ebad2
Yevrey <boyarskiykirill1@gmail.com>
2026-07-27 18:51:11 +0300

    /

---
+++ northline-react/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header'
+
+function Home() {
+	return (
+		<>
+			<Header />
+
+			<main>Home Page</main>
+		</>
+	)
+}
+
+export default Home
---
+++ northline-react/src/sections/HeroSection.jsx
@@ -0,0 +1,5 @@
+function HeroSection() {
+	return <main>Hero Section</main>
+}
+
+export default HeroSection
