26d7ffd1f6bcc4a33ddd7733bb704bd0b0903e49
Yevrey <boyarskiykirill1@gmail.com>
2026-07-27 16:46:56 +0300

    fix:import in main.jsx

--- northline-react/src/main.jsx
+++ northline-react/src/main.jsx
@@ -1,8 +1,7 @@
-import ' ./styles/main.scss'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './styles/main.scss'
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
