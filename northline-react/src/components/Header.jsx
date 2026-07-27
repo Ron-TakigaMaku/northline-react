ffe4473a8f15ce239638f60ea2a33875b5f25692
Yevrey <boyarskiykirill1@gmail.com>
2026-07-27 18:23:25 +0300

    add:pages

--- northline-react/src/components/Header.jsx
+++ northline-react/src/components/Header.jsx
@@ -1,7 +1,35 @@
 function Header() {
 	return (
-		<header>
-			<h1>NORTHLINE</h1>
+		<header className='header'>
+			<div className='container'>
+				<div className='header__container'>
+					<span className='header__logo'>NORTHLINE</span>
+					<nav className='header__nav'>
+						<ul className='header__list'>
+							<li className='header__item'>
+								<a href='#' className='header__link'>
+									Fleece
+								</a>
+							</li>
+							<li className='header__item'>
+								<a href='#' className='header__link'>
+									Bottoms
+								</a>
+							</li>
+							<li className='header__item'>
+								<a href='#' className='header__link'>
+									Accessories
+								</a>
+							</li>
+							<li className='header__item'>
+								<a href='#' className='header__link'>
+									Footwear
+								</a>
+							</li>
+						</ul>
+					</nav>
+				</div>
+			</div>
 		</header>
 	)
 }
