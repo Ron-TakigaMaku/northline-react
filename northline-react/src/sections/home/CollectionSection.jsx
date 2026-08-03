8a0dd9b2f306571828b2fa8f64e55cffaf3883bf
Yevrey <boyarskiykirill1@gmail.com>
2026-08-03 18:12:54 +0300

    /

--- northline-react/src/sections/home/CollectionSection.jsx
+++ northline-react/src/sections/home/CollectionSection.jsx
@@ -8,20 +8,20 @@ function CollectionSection() {
 
 	return (
 		<section className='collection reveal'>
-			<section className='collection__list'>
-				<section className='collection__content'>
-					<div className='collection__img'>
-						<img src={item.img} alt={item.title} />
-					</div>
-					<h1 className='collection__title'>{item.title}</h1>
-					<p className='collection__text'>{item.description}</p>
-					<section className='collection-actions'>
-						<a href='#story' className='btn'>
-							Details
-						</a>
-					</section>
+			{/* <section className='collection__list'> */}
+			<section className='collection__content'>
+				<div className='collection__img'>
+					<img src={item.img} alt={item.title} />
+				</div>
+				<h1 className='collection__title'>{item.title}</h1>
+				<p className='collection__text'>{item.description}</p>
+				<section className='collection__actions'>
+					<a href='#story' className='btn'>
+						Details
+					</a>
 				</section>
 			</section>
+			{/* </section> */}
 		</section>
 	)
 }
