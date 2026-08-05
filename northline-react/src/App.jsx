1bbf379fffc1908a0ddca5f9aaf8a5e54cd4d391
Yevrey <boyarskiykirill1@gmail.com>
2026-08-05 20:34:52 +0300

    /

--- northline-react/src/App.jsx
+++ northline-react/src/App.jsx
@@ -6,7 +6,6 @@ import Bottoms from '@/pages/Bottoms'
 import Fleece from '@/pages/Fleece'
 import Footwear from '@/pages/Footwear'
 import Home from '@/pages/Home'
-import Shop from '@/pages/Shop'
 import { Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -16,7 +15,6 @@ function App() {
 			<Header />
 			<Routes>
 				<Route path='/' element={<Home />} />
-				<Route path='/shop' element={<Shop />} />
 				<Route path='/fleece' element={<Fleece />} />
 				<Route path='/bottoms' element={<Bottoms />} />
 				<Route path='/accessories' element={<Accessories />} />
