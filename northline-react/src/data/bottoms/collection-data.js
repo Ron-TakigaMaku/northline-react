9db3e089d1233ad87ce87f97fbab3504e21c958c
Yevrey <boyarskiykirill1@gmail.com>
2026-09-23 15:32:24 +0300

    add:bottoms page

---
+++ northline-react/src/data/bottoms/collection-data.js
@@ -0,0 +1,11 @@
+import collectionImg from '@/assets/image/bottoms/collection/black_mountain.jpg'
+
+export const collectionData = [
+	{
+		title: 'SPRING / SUMMER 2026 COLLECTION',
+		description: 'Built for movement. Designed for exploration.',
+		img: collectionImg,
+	},
+]
+
+export default collectionData
---
+++ northline-react/src/data/bottoms/featured-data.js
@@ -0,0 +1,45 @@
+import Aphrodite_Pants from '@/assets/image/bottoms/featured/Aphrodite_Arise_Wide-Leg_Pants.jpg'
+import Basin_Skort from '@/assets/image/bottoms/featured/Basin_Skort.jpg'
+import Freedom_Pants from '@/assets/image/bottoms/featured/Freedom_Insulated_Pants.jpg'
+import Summit_Pacesetter from '@/assets/image/bottoms/featured/Summit_Series_Pacesetter.jpg'
+
+const featuredData = [
+	{
+		id: 1,
+		number: '01',
+		title: 'Women’s Aphrodite Arise Wide-Leg Pants',
+		text: 'Ultra-comfortable, wide-leg trail pants featuring a water-repellent DWR finish and UPF 40+ sun protection...',
+		description:
+			'Ultra-comfortable, wide-leg trail pants featuring a water-repellent DWR finish and UPF 40+ sun protection. The lightweight, moisture-wicking stretch fabric makes them a go-to choice for both active days and casual city lounging.',
+		img: Aphrodite_Pants,
+	},
+	{
+		id: 2,
+		number: '02',
+		title: "Women’s Summit Series Pacesetter 3'' Shorts",
+		text: 'Elite-level running shorts engineered for trail and ultra-marathons...',
+		description:
+			'Elite-level running shorts engineered for trail and ultra-marathons.Built with durable FlashDry-XD™ fabrics to wick away sweat and speed up drying time. Features a secure-zip waistband pocket setup for essential on-the-go storage.',
+		img: Summit_Pacesetter,
+	},
+	{
+		id: 3,
+		number: '03',
+		title: 'Women’s Freedom Insulated Pants',
+		text: 'Classic all-mountain ski and snowboard pants designed to block out winter weather...',
+		description:
+			'Classic all-mountain ski and snowboard pants designed to block out winter weather. Featuring a waterproof, breathable DryVent™ 2L shell and eco-conscious Heatseeker™ Eco insulation to keep you warm and dry from first lift to last call.',
+		img: Freedom_Pants,
+	},
+	{
+		id: 4,
+		number: '04',
+		title: 'Women’s Basin Skort',
+		text: 'Combining the feminine style of a skirt with the functional utility of hiking shorts...',
+		description:
+			'Combining the feminine style of a skirt with the functional utility of hiking shorts. Crafted from a highly durable, abrasion-resistant stretch-nylon blend with a built-in liner to ensure maximum mobility and confidence on any rugged trail.',
+		img: Basin_Skort,
+	},
+]
+
+export default featuredData
