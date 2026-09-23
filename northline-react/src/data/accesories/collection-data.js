b63a43182c127b51db468c3b21894d6e73d87f25
Yevrey <boyarskiykirill1@gmail.com>
2026-09-23 17:14:59 +0300

    add:collection in accesories page

--- northline-react/src/data/accesories/collection-data.js
+++ northline-react/src/data/accesories/collection-data.js
@@ -1,3 +1,11 @@
-const collectionData = []
+import collectionImg from '@/assets/image/accesories/collection/mountain.jpg'
+
+const collectionData = [
+	{
+		title: 'SPRING / SUMMER 2026 COLLECTION',
+		description: 'Built for movement. Designed for exploration.',
+		img: collectionImg,
+	},
+]
 
 export default collectionData
--- northline-react/src/pages/Accessories.jsx
+++ northline-react/src/pages/Accessories.jsx
@@ -1,8 +1,8 @@
-// import collectionData from '@/data/accesories/collection-data'
+import collectionData from '@/data/accesories/collection-data'
 import featuredData from '@/data/accesories/featured-data'
 import heroData from '@/data/accesories/hero-data'
 // import shopData from '@/data/accesories/shop-data'
-// import CollectionSection from '@/sections/home/CollectionSection'
+import CollectionSection from '@/sections/home/CollectionSection'
 import HeroSection from '@/sections/home/HeroSection'
 import FeaturedSection from '@/sections/shop/FeaturedSection'
 // import ShopSection from '@/sections/shop/ShopSection'
@@ -11,8 +11,8 @@ function Accessories() {
 	return (
 		<>
 			<HeroSection data={heroData} targetId='collection' />
-			{/* <CollectionSection data={collectionData} /> */}
 			<FeaturedSection data={featuredData} />
+			<CollectionSection data={collectionData} />
 			{/* <ShopSection data={shopData} /> */}
 		</>
 	)
