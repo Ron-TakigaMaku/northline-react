64f6a3100de1c7f8b9d4b038d6ad360bc6ff9466
Yevrey <boyarskiykirill1@gmail.com>
2026-09-24 14:00:53 +0300

    fix:photo

---
+++ northline-react/src/data/footwear/featured-data.js
@@ -0,0 +1,41 @@
+import
+
+const featuredData = [
+	{
+		id: 1,
+		number: '01',
+		title: 'Women’s VECTIV Enduris 4 Shoes',
+		text: 'Maximum support for every mile.',
+		description:
+			'Engineered for long-distance trail running, these shoes feature enhanced stability and superior cushioning to keep you comfortable on any terrain.',
+		img: '../img/footwear/featured/1.png',
+	},
+	{
+		id: 2,
+		number: '02',
+		title: 'Women’s Skeena Platform Sandals',
+		text: 'Elevated style, everyday comfort.',
+		description:
+			'Lightweight and durable, these platform sandals offer a perfect blend of modern aesthetics and functionality for city streets and seaside getaways.',
+		img: '../img/footwear/featured/2.png',
+	},
+	{
+		id: 3,
+		number: '03',
+		title: 'Women’s Never Stop Cush Slides',
+		text: 'Pure comfort for post-workout recovery.',
+		description:
+			'Designed for ultimate relaxation, these cushioned slides provide cloud-like comfort to soothe your feet after a challenging workout.',
+		img: '../img/footwear/featured/3.png',
+	},
+	{
+		id: 4,
+		number: '04',
+		title: 'Women’s Altamesa 300 V2 Shoes',
+		text: 'Confidence on every trail.',
+		description:
+			'Versatile performance footwear with exceptional grip and stability, built for those who demand reliability and style on their outdoor adventures.',
+		img: '../img/footwear/featured/4.png',
+	},
+]
+export default featuredData
--- northline-react/src/pages/Footwear.jsx
+++ northline-react/src/pages/Footwear.jsx
@@ -1,8 +1,21 @@
+// import collectionData from '@/data/bottoms/collection-data'
+// import featuredData from '@/data/bottoms/featured-data'
 import heroData from '@/data/footwear/hero-data'
+// import shopData from '@/data/bottoms/shop-data'
+// import CollectionSection from '@/sections/home/CollectionSection'
 import HeroSection from '@/sections/home/HeroSection'
+// import FeaturedSection from '@/sections/shop/FeaturedSection'
+// import ShopSection from '@/sections/shop/ShopSection'
 
 function Footwear() {
-	return <HeroSection data={heroData} targetId='collection' />
+	return (
+		<>
+			<HeroSection data={heroData} targetId='featured' />
+			{/* <FeaturedSection data={featuredData} />
+				<CollectionSection data={collectionData} />
+				<ShopSection products={shopData} /> */}
+		</>
+	)
 }
 
 export default Footwear
