6a13ee63bf8e0dd476cdac2269aedfea025557c0
Yevrey <boyarskiykirill1@gmail.com>
2026-09-24 14:18:26 +0300

    add:collection section

---
+++ northline-react/src/data/footwear/collection-data.js
@@ -0,0 +1,12 @@
+import sectionImg from '@/assets/image/footwear/collection/1.jpg'
+
+export const collectionData = [
+	{
+		title: 'The new season collection is here',
+		description:
+			'Built for movement. Designed for exploration. Crafted to last. Discover the latest collection of footwear, apparel, and accessories for every adventure.',
+		img: sectionImg,
+	},
+]
+
+export default collectionData
--- northline-react/src/pages/Footwear.jsx
+++ northline-react/src/pages/Footwear.jsx
@@ -1,8 +1,8 @@
-// import collectionData from '@/data/footwear/collection-data'
+import collectionData from '@/data/footwear/collection-data'
 import featuredData from '@/data/footwear/featured-data'
 import heroData from '@/data/footwear/hero-data'
 // import shopData from '@/data/footwear/shop-data'
-// import CollectionSection from '@/sections/home/CollectionSection'
+import CollectionSection from '@/sections/home/CollectionSection'
 import HeroSection from '@/sections/home/HeroSection'
 import FeaturedSection from '@/sections/shop/FeaturedSection'
 // import ShopSection from '@/sections/shop/ShopSection'
@@ -12,8 +12,8 @@ function Footwear() {
 		<>
 			<HeroSection data={heroData} targetId='featured' />
 			<FeaturedSection data={featuredData} />
-			{/* <CollectionSection data={collectionData} />
-			<ShopSection products={shopData} /> */}
+			<CollectionSection data={collectionData} />
+			{/* <ShopSection products={shopData} /> */}
 		</>
 	)
 }
